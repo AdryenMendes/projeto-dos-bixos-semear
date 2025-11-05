c65328f9d35567967333649e60776871698b6bbc
Adryen Mendes <adryenmendes2016@gmail.com>
2025-11-05 12:29:08 -0300

    Update encoder_control.c

--- lib/encoders/encoder_control.c
+++ lib/encoders/encoder_control.c
@@ -1,65 +1,86 @@
-#include "encoder_control.h"
+#include "encoder.h"
 
-static const char *TAG = "ENCODER_CONTROL";
+const char *TAG_ENCODER = "Encoder";
 
-// Handles para as unidades PCNT
-static pcnt_unit_handle_t pcnt_unit_L = NULL;
-static pcnt_unit_handle_t pcnt_unit_R = NULL;
+//Configura e inicializa o encoder (retorna o 'pcnt_unit_handle_t' pronto)
+pcnt_unit_handle_t init_encoder(encoder_side_t encoder)
+{
+    pcnt_unit_handle_t selected_encoder = NULL;
 
-// Função para configurar um unico encoder
-static void init_single_encoder(encoder_side_t side) {
-    pcnt_unit_handle_t* p_pcnt_unit = (side == ENCODER_LEFT) ? &pcnt_unit_L : &pcnt_unit_R;
-    int pin_a = ENCODER_A_PIN(side);
-    int pin_b = ENCODER_B_PIN(side);
-
+    ESP_LOGI(TAG_ENCODER, "Install pcnt unit");
     pcnt_unit_config_t unit_config = {
-        .high_limit = PCNT_HIGH_LIMIT,
-        .low_limit = PCNT_LOW_LIMIT,
-    };
-    ESP_ERROR_CHECK(pcnt_new_unit(&unit_config, p_pcnt_unit));
 
+        .high_limit = PCNT_HIGH_LIMIT,
+        .low_limit = PCNT_LOW_LIMIT
+
+    };
+    ESP_ERROR_CHECK(pcnt_new_unit(&unit_config, &selected_encoder));
+
+    ESP_LOGI(TAG_ENCODER, "Set glich filter");
     pcnt_glitch_filter_config_t filter_config = {
+
         .max_glitch_ns = 1000,
     };
-    ESP_ERROR_CHECK(pcnt_unit_set_glitch_filter(*p_pcnt_unit, &filter_config));
+    ESP_ERROR_CHECK(pcnt_unit_set_glitch_filter(selected_encoder, &filter_config));
 
-    pcnt_chan_config_t chan_a_config = {
-        .edge_gpio_num = pin_a,
-        .level_gpio_num = pin_b,
+    ESP_LOGI(TAG_ENCODER, "Install pcnt channels");
+    pcnt_chan_config_t channel_a_config = {
+
+        .edge_gpio_num = ENCODER_INPUT_A(encoder),
+        .level_gpio_num = ENCODER_INPUT_B(encoder)
+
     };
-    pcnt_channel_handle_t pcnt_chan_a = NULL;
-    ESP_ERROR_CHECK(pcnt_new_channel(*p_pcnt_unit, &chan_a_config, &pcnt_chan_a));
+    pcnt_channel_handle_t pcnt_channel_a = NULL;
+    ESP_ERROR_CHECK(pcnt_new_channel(selected_encoder, &channel_a_config, &pcnt_channel_a));
+
+    pcnt_chan_config_t channel_b_config = {
+
+        .edge_gpio_num = ENCODER_INPUT_B(encoder),
+        .level_gpio_num = ENCODER_INPUT_A(encoder)
+
+    };
+    pcnt_channel_handle_t pcnt_channel_b = NULL;
+    ESP_ERROR_CHECK(pcnt_new_channel(selected_encoder, &channel_b_config, &pcnt_channel_b));
+
+    ESP_LOGI(TAG_ENCODER, "Set edge and level actions for pcnt channels");
+    ESP_ERROR_CHECK(pcnt_channel_set_edge_action(pcnt_channel_a, PCNT_CHANNEL_EDGE_ACTION_DECREASE, PCNT_CHANNEL_EDGE_ACTION_INCREASE));
+    ESP_ERROR_CHECK(pcnt_channel_set_level_action(pcnt_channel_a, PCNT_CHANNEL_LEVEL_ACTION_KEEP, PCNT_CHANNEL_LEVEL_ACTION_INVERSE));
+    ESP_ERROR_CHECK(pcnt_channel_set_edge_action(pcnt_channel_b, PCNT_CHANNEL_EDGE_ACTION_INCREASE, PCNT_CHANNEL_EDGE_ACTION_DECREASE));
+    ESP_ERROR_CHECK(pcnt_channel_set_level_action(pcnt_channel_b, PCNT_CHANNEL_LEVEL_ACTION_KEEP, PCNT_CHANNEL_LEVEL_ACTION_INVERSE));
+
+    ESP_LOGI(TAG_ENCODER, "Enable pcnt unit");
+    ESP_ERROR_CHECK(pcnt_unit_enable(selected_encoder));
+
+    ESP_LOGI(TAG_ENCODER, "Clear pcnt unit");
+    ESP_ERROR_CHECK(pcnt_unit_clear_count(selected_encoder));
+
+    ESP_LOGI(TAG_ENCODER, "Start pcnt unit");
+    ESP_ERROR_CHECK(pcnt_unit_start(selected_encoder));
+
+    return selected_encoder;
+}
+
+bool pcnt_on_reach(pcnt_unit_handle_t unit, const pcnt_watch_event_data_t *edata, void *user_ctx)
+{
+    BaseType_t high_task_wakeup;
+    QueueHandle_t queue = (QueueHandle_t)user_ctx;
+
+    xQueueSendFromISR(queue, &(edata ->watch_point_value), &high_task_wakeup);
+
+    return (high_task_wakeup == pdTRUE);
+}
+
+//Retorna os pulsos contados
+float pulse_count(pcnt_unit_handle_t encoder)
+{
+    int pulse_count = 0;
+
+    pcnt_unit_clear_count(encoder);
     
-    pcnt_chan_config_t chan_b_config = {
-        .edge_gpio_num = pin_b,
-        .level_gpio_num = pin_a,
-    };
-    pcnt_channel_handle_t pcnt_chan_b = NULL;
-    ESP_ERROR_CHECK(pcnt_new_channel(*p_pcnt_unit, &chan_b_config, &pcnt_chan_b));
+    vTaskDelay(pdMS_TO_TICKS(50));
 
-    ESP_ERROR_CHECK(pcnt_channel_set_edge_action(pcnt_chan_a, PCNT_CHANNEL_EDGE_ACTION_DECREASE, PCNT_CHANNEL_EDGE_ACTION_INCREASE));
-    ESP_ERROR_CHECK(pcnt_channel_set_level_action(pcnt_chan_a, PCNT_CHANNEL_LEVEL_ACTION_KEEP, PCNT_CHANNEL_LEVEL_ACTION_INVERSE));
-    ESP_ERROR_CHECK(pcnt_channel_set_edge_action(pcnt_chan_b, PCNT_CHANNEL_EDGE_ACTION_INCREASE, PCNT_CHANNEL_EDGE_ACTION_DECREASE));
-    ESP_ERROR_CHECK(pcnt_channel_set_level_action(pcnt_chan_b, PCNT_CHANNEL_LEVEL_ACTION_KEEP, PCNT_CHANNEL_LEVEL_ACTION_INVERSE));
-
-    ESP_ERROR_CHECK(pcnt_unit_enable(*p_pcnt_unit));
-    ESP_ERROR_CHECK(pcnt_unit_clear_count(*p_pcnt_unit));
-    ESP_ERROR_CHECK(pcnt_unit_start(*p_pcnt_unit));
-}
-
-void encoders_init(void) {
-    ESP_LOGI(TAG, "Inicializando encoders...");
-    init_single_encoder(ENCODER_LEFT);
-    init_single_encoder(ENCODER_RIGHT);
-    ESP_LOGI(TAG, "Encoders inicializados.");
-}
-
-esp_err_t encoder_get_count(encoder_side_t side, int *count) {
-    pcnt_unit_handle_t unit = (side == ENCODER_LEFT) ? pcnt_unit_L : pcnt_unit_R;
-    return pcnt_unit_get_count(unit, count);
-}
-
-esp_err_t encoder_clear_count(encoder_side_t side) {
-    pcnt_unit_handle_t unit = (side == ENCODER_LEFT) ? pcnt_unit_L : pcnt_unit_R;
-    return pcnt_unit_clear_count(unit);
+    ESP_ERROR_CHECK(pcnt_unit_get_count(encoder, &pulse_count));    //ticks/0,05s
+    ESP_LOGI(TAG_ENCODER, "%f", pulse_count*0.00475*0.05);          //(ticks/0,05s)*(rad/ticks)*0,05 = rad/s
+    
+    return pulse_count;
 }
